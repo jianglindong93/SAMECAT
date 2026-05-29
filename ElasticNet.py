4cc2a79172f479e62128d5d54b044d9d9bfa5f23
LJiang <49157532+jianglindong93@users.noreply.github.com>
2026-05-29 17:01:13 -0500

    Update ElasticNet.py

--- ElasticNet.py
+++ ElasticNet.py
@@ -321,8 +321,6 @@ for bmd_site in bmd_site_list:
         summarized_results_cache.to_csv(summarized_results_path + PB + "_" + bmd_site + "_en_summarized_results.csv", index=False)
 
 
-# In[ ]:
-
 
 
 
