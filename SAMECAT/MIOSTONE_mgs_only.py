3d9aab055f8e3914184022b6b3406e5df713be96
LJiang <49157532+jianglindong93@users.noreply.github.com>
2026-06-01 22:08:18 -0500

    Update and rename taxo_mv_bmd_mgs_only.py to MIOSTONE_mgs_only.py

--- SAMECAT/MIOSTONE_mgs_only.py
+++ SAMECAT/MIOSTONE_mgs_only.py
@@ -802,8 +802,6 @@ summarized_results_cache = pd.concat([div_tract_cache, summarized_results_cache]
 summarized_results_cache.to_csv(summarized_results_path + bmd_site + '_miostone_mgs_only_summarized_results.csv', index=False)
 
 
-# In[ ]:
-
 
 
 
